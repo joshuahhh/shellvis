b28fe47f632213c59a7283e1c48bae355a05a791
Joshua Horowitz <joshuah@alum.mit.edu>
2024-02-19 18:12:57 -0800

    commit

--- src/TraceInPlaceAndGridV.tsx
+++ src/TraceInPlaceAndGridV.tsx
@@ -7,7 +7,7 @@ export const TraceInPlaceAndGridV = memo((props: TraceVProps) => {
   const { trace, script } = props;
   const { detailsMode } = useContext(HVContext);
 
-  return <div className={`trace detailsMode-${detailsMode}`} style={{display: 'flex', flexDirection: 'column'}}>
+  return <div className={`trace detailsMode-${detailsMode}`}>
     {script.lineTree.map((node, i) =>
       <LineTreeNodeV key={i} script={script} trace={trace} node={node} context=""/>
     )}
