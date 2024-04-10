00d4543dca3ffdd93d0d953d2f3811031b532ed5
Joshua Horowitz <joshuah@alum.mit.edu>
2024-04-09 18:21:52 -0700

    innocuous gridtest stuff

--- src/client/GridTest.tsx
+++ src/client/GridTest.tsx
@@ -1,19 +1,22 @@
-import { memo } from 'react';
+import { memo, useCallback, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export const GridTest = memo(() => {
-  const widths = [200, 400, 600, 800];
+  const widths = [100, 200, 400];
+
+  const [ gTC, setGTC ] = useState('fit-content(0%)_minmax(0,max-content)_minmax(min-content,max-content)_1fr');
 
   return <div className='px-16 mt-4'>
    <h1 className='text-4xl mb-6 text-gray-300'>grid test</h1>
    <p>ok this is wip ok</p>
+   <pre></pre>
    {widths.map((width, i) =>
-      <div key={i} style={{width, maxWidth: width}}>
+      <div key={i} style={{width, maxWidth: width}} className='bg-gray-600'>
         <div className='inline-grid grid-cols-[fit-content(0%)_minmax(0,max-content)_minmax(min-content,max-content)_1fr]'>
           <TrackLabels numTracks={4}/>
-          <TestBlock width={40} className='bg-blue-500'/>
-          <TestBlock width={400} minWidth={0} className='bg-orange-500'/>
-          <TestBlock width={200} minWidth={0} className='bg-indigo-500 col-span-2'/>
+          <TestBlock className='bg-blue-500'>hi</TestBlock>
+          <TestBlock className='mt-6 bg-orange-500'>helloooooooooooo there</TestBlock>
+          <TestBlock className='mt-12 bg-indigo-500'>how are you</TestBlock>
         </div>
       </div>
     )}
@@ -25,29 +28,48 @@ const TrackLabels = memo((props: {
 }) => {
   const { numTracks } = props;
   return Array.from({ length: numTracks }).map((_, i) => {
-    return <div key={i} className='text-gray-300 text-sm text-center border-r-2'>
-      {i}
+    return <div key={i} className='text-gray-300 text-sm text-center relative h-6'>
+      <Disconnect>{i}</Disconnect>
+      <div className='absolute right-0 w-px h-full bg-gray-300'/>
     </div>;
   });
 });
 
-const TestBlock = memo((props: {
-  width: number,
-  minWidth?: number,
-  className?: string,
+// MUST set parent to relative
+const Disconnect = memo((props: {
+  children?: React.ReactNode,
 }) => {
-  const { width, minWidth = width, className } = props;
-  const extra = width - minWidth;
-  return <div className={twMerge('h-6 rounded flex bg-slate-500', className)} style={{ width, minWidth }}>
-    { minWidth > 0 &&
-      <div className='px-1 border-r-2' style={{ width: minWidth }}>
-        {minWidth}
-      </div>
-    }
-    { extra > 0 &&
-      <div className='px-1 flex-grow'>
-        {width - minWidth}
-      </div>
-    }
+  return <div className='absolute left-0 top-0'>
+    {props.children}
   </div>;
 });
+
+const TestBlock = memo((props: {
+  children?: React.ReactNode,
+  className?: string,
+}) => {
+  const { className } = props;
+  const [ widthStats, setWidthStats ] = useState<ReturnType<typeof getWidthStats> | null>(null);
+  const setDiv = useCallback((elem: HTMLDivElement | null) => {
+    if (!elem) { return; }
+    setWidthStats(getWidthStats(elem));
+  }, []);
+  return <div ref={setDiv} className={twMerge('h-6 rounded flex bg-slate-500 relative overflow-y-clip', className)}>
+    {props.children}
+    {widthStats && <div className='absolute w-px h-full bg-gray-300' style={{left: widthStats.minContent - 1}}/>}
+    {widthStats && <div className='absolute w-px h-full bg-gray-300' style={{left: widthStats.maxContent - 1}}/>}
+  </div>;
+});
+
+export function getWidthStats(elem: HTMLElement): { minContent: number, maxContent: number } {
+  const clone = elem.cloneNode(true) as HTMLElement;
+  clone.style.position = 'absolute';
+  clone.style.visibility = 'hidden';
+  clone.style.width = 'min-content';
+  document.body.appendChild(clone);
+  const minContent = clone.offsetWidth;
+  clone.style.width = 'max-content';
+  const maxContent = clone.offsetWidth;
+  clone.remove();
+  return { minContent, maxContent };
+}
